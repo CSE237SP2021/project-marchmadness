81bc12d942c98304b4dd11b81a1c6e28d122f090
Jeff Candell <jcandell@wustl.edu>
2021-03-29 11:58:53 -0500

    Bracket

--- MarchMadness/src/marchMadnessBracket/Bracket.java
+++ MarchMadness/src/marchMadnessBracket/Bracket.java
@@ -1,6 +1,12 @@
 package marchMadnessBracket;
 
-public class bracket {
+public class Bracket {
+	
+	Region[] regions;
+	public Bracket() {
+		regions = new Region[4];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}

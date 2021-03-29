976991d5246b9c69bc4f5b76161697e334786a92
thevincentgao <g.vincent@wustl.edu>
2021-03-28 22:04:36 -0500

    viewOtherBrackets without user,bracket classes created

--- MarchMadness/src/marchMadnessBracket/viewOtherBrackets.java
+++ MarchMadness/src/marchMadnessBracket/viewOtherBrackets.java
@@ -1,5 +1,15 @@
 package marchMadnessBracket;
 
 public class viewOtherBrackets {
-
+	//instantiate users and bracket
+	User bracketUser = new User();
+	
+	Bracket bracketToShow = new Bracket();
+	
+	public Bracket viewOtherBrackets(User toViewUser){
+		bracketUser = toViewUser;
+		
+		bracketToShow = bracketUser.getBracket();
+		bracketToShow.displayBracket();
+	}
 }
